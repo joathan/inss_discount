e5fcec7ed47f4131dddb704e300010f067850d73
Joathan Souza <joathanf@gmail.com>
2024-04-12 18:45:46 -0300

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_12_184941) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_12_185701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "proponents", force: :cascade do |t|
+    t.string "name"
+    t.string "cpf"
+    t.jsonb "address"
+    t.integer "salary"
+    t.integer "inss_discount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
