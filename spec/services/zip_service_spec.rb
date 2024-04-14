203e692bd6e07e800a5215a9f9d63527a0d9b5e6
Joathan Souza <joathanf@gmail.com>
2024-04-14 02:29:24 -0300

    implementing rspec test zip_service

---
+++ spec/services/zip_service_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ZipService, type: :service do
+  describe '#call' do
+    let(:zip) { '01001-000' }
+    let(:service) { ZipService.new(zip) }
+
+    context 'when the zip code is valid' do
+      before do
+        stub_request(:get, "https://viacep.com.br/ws/#{zip}/json")
+          .to_return(status: 200, body: { logradouro: 'Praça da Sé', localidade: 'São Paulo' }.to_json)
+      end
+
+      it 'returns the address information' do
+        result = service.call
+        expect(result['logradouro']).to eq('Praça da Sé')
+        expect(result['localidade']).to eq('São Paulo')
+      end
+    end
+
+    context 'when the zip code is invalid' do
+      before do
+        stub_request(:get, "https://viacep.com.br/ws/#{zip}/json")
+          .to_return(status: 400)
+      end
+
+      it 'raises a StandardError with "CEP inválido"' do
+        expect { service.call }.to raise_error(StandardError, 'CEP inválido')
+      end
+    end
+
+    context 'when there is a timeout' do
+      before do
+        stub_request(:get, "https://viacep.com.br/ws/#{zip}/json")
+          .to_raise(Timeout::Error)
+      end
+
+      it 'raises a StandardError with "Tempo de requisição excedido"' do
+        expect { service.call }.to raise_error(StandardError, 'Tempo de requisição excedido')
+      end
+    end
+  end
+end
