491f01937bda046b7b5ebeee8097602bc2b3d5fe
Joathan Souza <joathanf@gmail.com>
2024-04-14 00:55:15 -0300

    refactoring Model Proponent

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -1,35 +1,26 @@
 # frozen_string_literal: true
 
+# Path: app/models/proponent.rb
 class Proponent < ApplicationRecord
   store_accessor :address, :zip, :street, :city, :state, :number
 
   has_many :contacts, dependent: :destroy
   accepts_nested_attributes_for :contacts, reject_if: :all_blank, allow_destroy: true
 
-  before_save :remove_salary_formatting
-
   def formatted_salary
-    if salary.present?
-      salary_in_reais = salary / 100.0
-      ActionController::Base.helpers.number_to_currency(salary_in_reais, unit: 'R$', separator: ',', delimiter: '.', format: '%u %n')
-    else
-      nil
-    end
+    return nil if salary.blank?
+
+    salary_in_reais = salary / 100.0
+    ActionController::Base.helpers.number_to_currency(salary_in_reais, unit: 'R$', separator: ',', delimiter: '.',
+                                                                       format: '%u %n')
   end
 
   def salary=(value)
-    value = value.to_s.gsub(/[^\d]/, '').to_i
-
-    self[:salary] = value
+    cleaned_salary = value.to_s.gsub(/[^\d]/, '').to_i
+    super(cleaned_salary)
   end
 
   def address_formatted
     "#{street}, nº #{number}, #{city} - #{state}, #{zip}"
-  end
-
-  private
-
-  def remove_salary_formatting
-    self.salary = salary.to_s.gsub(/[^\d]/, '').to_i
   end
 end
