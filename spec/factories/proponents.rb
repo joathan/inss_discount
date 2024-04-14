a9f332600837020ef3fc5081e43fd3d3ef3dca01
Joathan Souza <joathanf@gmail.com>
2024-04-14 00:51:38 -0300

    implementing factory proponent

--- spec/factories/proponents.rb
+++ spec/factories/proponents.rb
@@ -1,10 +1,17 @@
 FactoryBot.define do
   factory :proponent do
-    name { "MyString" }
-    cpf { "MyString" }
-    address { "" }
-    contacts { "" }
-    salary { 1 }
-    inss_discount { 1 }
+    name { Faker::Name.name }
+    cpf { Faker::IdNumber.brazilian_citizen_number }
+    address do
+      {
+        zip: Faker::Address.zip_code,
+        street: Faker::Address.street_name,
+        city: Faker::Address.city,
+        state: Faker::Address.state_abbr,
+        number: Faker::Address.building_number
+      }
+    end
+    salary { rand(100000..1500000) }
+    inss_discount { rand(10000..100000) }
   end
 end
