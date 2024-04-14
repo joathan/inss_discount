84c7336f543be5874867c638b0c8a83c85bd4ef6
Joathan Souza <joathanf@gmail.com>
2024-04-14 18:37:51 -0300

    implementing pier chart in index

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -7,6 +7,14 @@ class Proponent < ApplicationRecord
   has_many :contacts, dependent: :destroy
   accepts_nested_attributes_for :contacts, reject_if: :all_blank, allow_destroy: true
 
+  def self.salary_distribution
+    select(
+      "(CASE WHEN salary / 100.0 <= 1045 THEN 'Até R$ 1.045,00' WHEN salary / 100.0 > 1045 AND salary / 100.0 <= 2089.60 THEN 'De R$ 1.045,01 a R$ 2.089,60' WHEN salary / 100.0 > 2089.60 AND salary / 100.0 <= 3134.40 THEN 'De R$ 2.089,61 até R$ 3.134,40' WHEN salary / 100.0 > 3134.40 AND salary / 100.0 <= 6101.06 THEN 'De R$ 3.134,41 até R$ 6.101,06' ELSE 'Acima de R$ 6.101,06' END) as salary_range, COUNT(*) as count")
+    .group('salary_range')
+    .order(Arel.sql('MIN(salary / 100.0)'))
+    .each_with_object({}) { |proponent, hash| hash[proponent.salary_range] = proponent.count }
+  end
+
   def formatted_salary
     return nil if salary.blank?
 
