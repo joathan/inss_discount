0cc48b1f09625a5a536047d5cad0aa0d4342bab9
Joathan Souza <joathanf@gmail.com>
2024-04-14 02:34:02 -0300

    refactoring rspec test zip_service

--- spec/services/zip_service_spec.rb
+++ spec/services/zip_service_spec.rb
@@ -3,18 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe ZipService, type: :service do
+  subject(:result) { service.call }
+
   describe '#call' do
     let(:zip) { '01001-000' }
+    let(:api_url) { "https://viacep.com.br/ws/#{zip}/json" }
     let(:service) { ZipService.new(zip) }
 
     context 'when the zip code is valid' do
       before do
-        stub_request(:get, "https://viacep.com.br/ws/#{zip}/json")
+        stub_request(:get, api_url)
           .to_return(status: 200, body: { logradouro: 'Praça da Sé', localidade: 'São Paulo' }.to_json)
       end
 
       it 'returns the address information' do
-        result = service.call
         expect(result['logradouro']).to eq('Praça da Sé')
         expect(result['localidade']).to eq('São Paulo')
       end
@@ -22,23 +24,23 @@ RSpec.describe ZipService, type: :service do
 
     context 'when the zip code is invalid' do
       before do
-        stub_request(:get, "https://viacep.com.br/ws/#{zip}/json")
+        stub_request(:get, api_url)
           .to_return(status: 400)
       end
 
       it 'raises a StandardError with "CEP inválido"' do
-        expect { service.call }.to raise_error(StandardError, 'CEP inválido')
+        expect { result }.to raise_error(StandardError, 'CEP inválido')
       end
     end
 
     context 'when there is a timeout' do
       before do
-        stub_request(:get, "https://viacep.com.br/ws/#{zip}/json")
+        stub_request(:get, api_url)
           .to_raise(Timeout::Error)
       end
 
       it 'raises a StandardError with "Tempo de requisição excedido"' do
-        expect { service.call }.to raise_error(StandardError, 'Tempo de requisição excedido')
+        expect { result }.to raise_error(StandardError, 'Tempo de requisição excedido')
       end
     end
   end
