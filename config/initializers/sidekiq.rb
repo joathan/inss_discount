e3d2b6d181ba7f7429254b2da03d39ea19056c94
Joathan Souza <joathanf@gmail.com>
2024-04-15 00:59:35 -0300

    configuring sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,15 @@
+require 'sidekiq/web'
+
+sidekiq_config = { url: ENV['REDIS_URL'] }
+
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_config
+end
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [ENV['SIDEKIQ_USERNAME'], ENV['SIDEKIQ_PASSWORD']]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'inss/calculate'
-  get 'zip/search'
-  resources :proponents
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get 'inss/calculate'
+  get 'zip/search'
 
-  # Defines the root path route ("/")
+  resources :proponents
+  devise_for :users
+
   root "users#sign_in"
 end
