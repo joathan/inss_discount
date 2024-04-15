d084bb407db4bfb357902727d82fdc78d05cfc6d
Joathan Souza <joathanf@gmail.com>
2024-04-14 21:46:57 -0300

    implementing iMask, fixing and refactoring complements

--- app/helpers/proponents_helper.rb
+++ app/helpers/proponents_helper.rb
@@ -5,4 +5,28 @@ module ProponentsHelper
   def address_formatted(proponent)
     "#{proponent.street}, nº #{proponent.number}, #{proponent.city} - #{proponent.state}, #{proponent.zip}"
   end
+
+  def inss_discount_formatted(proponent)
+    return nil if proponent.inss_discount.blank?
+
+    inss_in_reais = proponent.inss_discount / 100.0
+    number_formatter(inss_in_reais)
+  end
+
+  def salary_formatted(proponent)
+    return nil if proponent.salary.blank?
+
+    salary_in_reais = proponent.salary / 100.0
+    number_formatter(salary_in_reais)
+  end
+
+  private
+
+  def number_formatter(value)
+    number_to_currency(value,
+                       unit: 'R$',
+                       separator: ',',
+                       delimiter: '.',
+                       format: '%u %n')
+  end
 end
