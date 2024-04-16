33b21a8a7214bc98ccc39a8f5aa38ea8e23a7eef
Joathan Souza <joathanf@gmail.com>
2024-04-16 01:09:52 -0300

    fixing rspec

--- spec/requests/zip_spec.rb
+++ spec/requests/zip_spec.rb
@@ -3,7 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Zips', type: :request do
-  describe 'GET /search' do
+  describe 'GET /search', :vcr do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+    end
+
     it 'returns http success' do
       get '/zip/search?zip=01311000'
 
