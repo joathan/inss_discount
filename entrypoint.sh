8e70c444cb7a72e27afcf7f3aca8ff32ec27ee83
Joathan Souza <joathanf@gmail.com>
2024-04-16 10:08:08 -0300

    startup settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,8 @@ bundle exec rails db:create
 # running migrations
 bundle exec rails db:migrate
 
+# running assets precompile
+bundle exec rails assets:precompile
+
 # running the server
 rm -f tmp/pids/server.pid && bundle exec rails s -p 8010 -b '0.0.0.0'
