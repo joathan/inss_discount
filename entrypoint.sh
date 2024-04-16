5ac9221304ef5bdc773399f935b9d3295afcf584
Joathan Souza <joathanf@gmail.com>
2024-04-16 09:49:45 -0300

    startup settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,9 @@ yarn install
 
 # creating a database
 bundle exec rails db:create
+
+# running migrations
+bundle exec rails db:migrate
+
+# running the server
+rm -f tmp/pids/server.pid && bundle exec rails s -p 8010 -b '0.0.0.0'
